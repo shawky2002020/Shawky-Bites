d91e02b638fd2316fd6092c345d0a8719653266f
Shawky Elsayed <shawkyelsayed2002@gmail.com>
2024-08-25 23:38:27 +0200

    final changes

--- frontend/src/app/components/pages/order-page/order-page.component.ts
+++ frontend/src/app/components/pages/order-page/order-page.component.ts
@@ -2,11 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ActivationEnd } from '@angular/router';
 import { OrderService } from '../../../services/order.service';
 import { Order } from '../../shared/models/order';
+import { trigger, transition, style, animate, group } from '@angular/animations';
 
 @Component({
   selector: 'app-order-page',
   templateUrl: './order-page.component.html',
-  styleUrls: ['./order-page.component.css']
+  styleUrls: ['./order-page.component.css'],
+  animations: [
+    trigger('fadeInAndScale', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'scale(0.8)' }),
+        group([
+          animate('500ms', style({ opacity: 1 })),
+          animate('800ms 500ms', style({ transform: 'scale(1)' })) // Delayed scale animation
+        ])
+      ])
+    ])
+  ]
 })
 export class OrderPageComponent implements OnInit {
 
