cc7251a5b73d92346daee401ebcf184c0bec2d13
Shawky Elsayed <shawkyelsayed2002@gmail.com>
2024-08-25 23:54:25 +0200

    modified:   backend/src/server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -27,7 +27,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname,'public', 'index.html'))
 })
 
-const port = 5000;
+const port =process.env.PORT ||5000;
 app.listen(port,()=>{
   console.log('Website served on port ',port);
   
